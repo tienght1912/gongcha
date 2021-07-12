ebc6d8c70dab1ccca2d012db6619800dd6d4be0a
Tiếng HT <tieng1912@gmail.com>
2021-07-12 19:34:03 +0700

    oke

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,17 +1,49 @@
 import React from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
 import Home from './Home'
 
 export default function Login({ navigation }) {
     return (
         <View style={styles.container}>
-            
+            <View style={styles.formLogin}>
+                <TextInput
+                style={styles.formUserName}
+                    // placeHolder="Enter username"
+                />
+            </View>
+            <TouchableOpacity
+                style={styles.btnLogin}
+                onPress={()=> navigation.navigate('Home')}
+            >
+                <Text style={styles.txtButton}>Go Home</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
-        backgroundColor:"#e17055"
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    formUserName:{
+        backgroundColor:"#ccc",
+        width:240,
+        height:40,
+        borderRadius:10,
+        borderWidth:1,
+        borderColor:"#000"
+    },
+    btnLogin: {
+        width: 140,
+        height: 40,
+        backgroundColor: "#e17055",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    txtButton: {
+        color: "#fff"
     }
 })

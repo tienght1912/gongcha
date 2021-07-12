38ab412cf5de1a31ef3fefee09b2da7450e4370b
Tiếng HT <tieng1912@gmail.com>
2021-07-12 18:33:32 +0700

    oke

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import 'react-native-gesture-handler';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/Home';
+import LoginScreen from './src/screens/Login';
+import RegisterScreen from './src/screens/Register';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>App.js</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
